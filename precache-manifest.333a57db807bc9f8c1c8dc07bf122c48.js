e5be77a99f1bb59e172587179658d7262f0b74fc
hadiburpee <hadiburpee@gmail.com>
2019-02-01 15:57:08 -0800

    Updates

---
+++ precache-manifest.333a57db807bc9f8c1c8dc07bf122c48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a087fdcd5eb7398ef52",
+    "url": "/click/static/js/runtime~main.5a087fdc.js"
+  },
+  {
+    "revision": "862a58c6cce302fccf24",
+    "url": "/click/static/js/main.862a58c6.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/click/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "862a58c6cce302fccf24",
+    "url": "/click/static/css/main.499526a0.chunk.css"
+  },
+  {
+    "revision": "f2700e00a787255a4900e63ae8bb88b1",
+    "url": "/click/index.html"
+  }
+];
