f1a93ec4120e00e046ad312214396d5a4436565c
hadiburpee <hadiburpee@gmail.com>
2019-02-01 00:19:17 -0800

    score clicker works

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React, {Component} from 'react';
 import Card from './components/card/card';
 import cards from "./cards.json";
 import Container from './components/container/container';
+import Header from './components/header/header';
 
 
 
 
 class App extends Component {
   state = {
-    cards
+    cards,
+    score: 0
+
   }
 
+  incrementScore = () => {
+    this.setState( prevState => ({score: prevState.score + 1}));
+  }
+
+  resetScore = event => {
+    this.setState( prevState => ({score: prevState.score - prevState.score}));
+  }
 
   render() {
+    console.log(cards);
     return (
       <Container>
-      {this.state.cards.map( card =>
+      <Header>
+      Album Cover Click Game ||
+      Score: {this.state.score}
+      </Header>
+      {this.state.cards.map(card =>
         <Card 
         key={card.key}
         selected={card.selected}
         image={card.image}
         name={card.name}
+        score={this.incrementScore}
         />
       )}
       </Container>
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,11 +2,11 @@ import React from 'react';
 import "./card.css"
 
 
-function Card (props) {
+function Card (props) {    
         return(
             <div className="cardHolder">    
                 <span className="img-container">
-                    <img alt={props.name} src={props.image} />
+                    <img alt={props.name} src={props.image} onClick={props.score} />
                 </span>
 
 
